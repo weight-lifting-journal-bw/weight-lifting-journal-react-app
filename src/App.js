b007b298745006c39f4b9f3f35516861066f38ce
Akere Adetola <adetolaakere1@gmail.com>
2019-10-22 10:44:33 +0100

    register

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import React from 'react';
+import React from "react";
 
 import { Provider } from "react-redux";
 import { combineReducers, createStore } from "redux";
-
+import { Route, NavLink } from "react-router-dom";
 import * as reducers from "./state/reducers";
 
-import './App.css';
-import CountComponent from './CountComponent';
-import Exercises from './Components/Exercises';
+import "./App.css";
+import CountComponent from "./CountComponent";
+import Exercises from "./Components/Exercises";
+import Signup from "./Components/auth/Register";
 
 function App() {
-
   const rootReducer = combineReducers({
     count: reducers.countReducer,
-    exercises: reducers.exerciseReducer,
-  })
+    exercises: reducers.exerciseReducer
+  });
 
   const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
   console.log(store);
-  
 
   return (
-    <Provider store = {store}>
+    <Provider store={store}>
       <div className="App">
-      <CountComponent />
-      <Exercises />
+        <NavLink exact to="/signup" activeClassName="active">
+          register
+        </NavLink>
+        &nbsp;
+        <NavLink exact to="/count" activeClassName="active">
+          count
+        </NavLink>
+        &nbsp;
+        <NavLink exact to="/exercises" activeClassName="active">
+          Exercises
+        </NavLink>
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/count" component={CountComponent} />
+        <Route exact path="/exercises" component={Exercises} />
       </div>
     </Provider>
   );
