7a5c0892d0d937181a72c22e581a4a502042722d
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-23 11:01:44 +0100

    :lipstick: Improve Dashboard style

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import { connect } from "react-redux";
 import * as actionCreators from "../state/actionCreators";
 import Exercise from "./Exercise";
+import styled from "styled-components";
 
 export const Dashboard = ({getExercises, exercises}) => {
     const userId = localStorage.getItem('userID')
@@ -9,14 +10,22 @@ export const Dashboard = ({getExercises, exercises}) => {
         getExercises()
     }, [])
     return (
-        <div>
+        <StyledDiv>
             {
                 exercises
                 .filter(exercise => exercise.user_id == userId)
                 .map(exercise => <Exercise key={exercise.id} exercise={exercise} />)}
-        </div>
+        </StyledDiv>
     )
 }
+
+const StyledDiv = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    >div {
+        width: 25%;
+    }
+`
 
 export default connect(
     state => state,
