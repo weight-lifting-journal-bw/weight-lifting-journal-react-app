ba537b2c1e2ab99dba7edb01e59f7e501cda3cce
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-24 14:55:48 +0100

    :sparkles: Get Image Upload to work

--- src/Components/Exercise.js
+++ src/Components/Exercise.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+import { CloudinaryContext, Image } from "cloudinary-react";
 
 import * as actionCreators from "../state/actionCreators";
 import { Button, Card, Heading, Content } from "react-bulma-components";
 import styled from "styled-components";
 import "react-bulma-components/dist/react-bulma-components.min.css";
 import axiosWithAuth from "../utils/AxiosWithAuth";
+import { fetchPhotos } from "../utils/CloudinaryService";
+
+import { openUploadWidget } from "../utils/CloudinaryService";
 
 export function Exercise(props) {
   const { exercise, beginEdit, removeExercise } = props;
@@ -14,8 +18,12 @@ export function Exercise(props) {
   const startEdit = exercise => {
     beginEdit(exercise);
     console.log(exercise);
-    props.history.push("/exercises/add"); 
+    props.history.push("/exercises/add");
   };
+  const [imageIds, setIds] = useState([]);
+  useEffect(() => {
+    fetchPhotos(exercise.id, setIds);
+  }, []);
 
   const deleteExercise = id => {
     axiosWithAuth()
@@ -26,20 +34,54 @@ export function Exercise(props) {
       .catch(err => console.log(err));
   };
 
+  function uploadImageWithCloudinary() {
+    const uploadOptions = {
+      cloudName: "emkaydauda",
+      tags: [exercise.id, exercise.title],
+      uploadPreset: "presetOne"
+    };
+    openUploadWidget(uploadOptions, (error, photos) => {
+      if (!error) {
+        console.log(photos);
+      } else {
+        console.log(error);
+      }
+    });
+  }
+
   return (
-    <StyledCard>
-      <Heading>{title}</Heading>
-      <Content>{`Targeted Area: ${targeted_area}`}</Content>
-      <Content>{`Reps Completed: ${reps_completed}`}</Content>
-      <div>
-        <Button onClick={() => startEdit(exercise)} color="dark">
-          Edit
-        </Button>
-        <Button onClick={() => deleteExercise(exercise.id)} color="danger">
-          Delete
-        </Button>
-      </div>
-    </StyledCard>
+    <CloudinaryContext cloudName='emkaydauda'>
+      <StyledCard>
+        <Heading>{title}</Heading>
+        <Content>{`Targeted Area: ${targeted_area}`}</Content>
+        <Content>{`Reps Completed: ${reps_completed}`}</Content>
+
+        <div>
+          {
+            imageIds.map(image => 
+              <Image key = {image}
+            publicId={image}
+            fetch-format="auto"
+            quality="auto"
+          />
+              )
+          }
+        </div>
+
+        <div>
+          
+          <Button onClick={() => startEdit(exercise)} color="dark">
+            Edit
+          </Button>
+          <Button onClick={() => uploadImageWithCloudinary()} color="primary">
+            Upload Images
+          </Button>
+          <Button onClick={() => deleteExercise(exercise.id)} color="danger">
+            Delete
+          </Button>
+        </div>
+      </StyledCard>
+    </CloudinaryContext>
   );
 }
 
